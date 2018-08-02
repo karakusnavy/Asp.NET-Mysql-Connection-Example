075ccb628252326d3f2a70a7b2fd266b34ec42ae
Samed Karakus <karakusnavy@gmail.com>
2018-08-02 09:58:18 +0300

    ++
    Asp.net kullanılarak mysql bğalantısı yapıldı.

---
+++ SimpleMysqlConnectApplication/Index.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="SimpleMysqlConnectApplication.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Samed Karakuş</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Simple Mysql Application Status</h1><br />
+        <a runat="server" id="connectsuccess" style="display:none;color:green">Mysql Connect is Successful!</a>
+        <a runat="server" id="connectfailed" style="display:none;color:red">Mysql Connect Failed! </a>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SimpleMysqlConnectApplication/Index.aspx.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleMysqlConnectApplication
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        public MySqlConnection con = new MySqlConnection("Server=localhost;Database=databasename;Uid=username;Pwd=password;Encrypt=false;AllowUserVariables=True;UseCompression=True;Charset=utf8");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            if (con.State != ConnectionState.Closed)
+            {
+
+                connectsuccess.Style["display"] = "block";
+                con.Close();
+            }
+            else
+            {
+                connectfailed.Style["display"] = "block";                
+            }
+        }
+    }
+}
